0efb54fedc9bbb4609543a1fb068d976de58f17b
raptor764 <103536828+raptor764@users.noreply.github.com>
2022-06-01 21:04:08 +0300

    comments added

--- project.pl
+++ project.pl
@@ -42,25 +42,38 @@ light(point(8,6)).
 
 
 %rules
+
+%to check if a point is not a wall
 no_wall(point(R,C)):- not(wall(point(R,C))).
+
+%to check if a point is not a wall_num
 no_wall_num(point(R,C),_):- not(wall_num(point(R,C))).
+
+%to check if a point is inside the boudaries of the board
 inside_bounds(R,C):- size(Rmax,Cmax),R > 0,C > 0,R=<Rmax,C=<Cmax.
 
+%to check if the right neighbour of a point is not a wall
 right_empty(R,C):-size(_,Cmax), inside_bounds(R,C),
                     C1 is C + 1 , C1 =< Cmax ,
                     no_wall(point(R,C1)) , no_wall_num(point(R,C1),_).
+
+%to check if the left neighbour of a point is not a wall
 left_empty(R,C):- inside_bounds(R,C),
                     C1 is C - 1 , C1 > 0, 
                     no_wall(point(R,C1)) , no_wall_num(point(R,C1),_).
+
+%to check if the top neighbour of a point is not a wall
 top_empty(R,C):- inside_bounds(R,C), 
                     R1 is R - 1 ,R1 > 0, 
                     no_wall(point(R1,C)) , no_wall_num(point(R1,C),_).
+
+%to check if the bottom neighbour of a point is not a wall
 down_empty(R,C):- size(Rmax,_),inside_bounds(R,C), 
                     R1 is R + 1 ,R1 =< Rmax, 
                     no_wall(point(R1,C)) , no_wall_num(point(R1,C),_).
 
 
-%check if four sides empty and num is four then it is ready to be lit
+%to check if four sides empty and num is four then it is ready to be lit
 sides_empty(R,c):- right_empty(R,C),left_empty(R,C),top_empty(R,C),down_empty(R,C).
 
 
